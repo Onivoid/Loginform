299dc0ef3a77b62b15a46811083d5d8739f70899
Skullyfox <Skullyfox.yt@gmail.com>
2018-04-13 22:16:47 +0200

    UPDATE

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const   gulp            = require('gulp'),
+        sass            = require('gulp-sass'),
+        concat          = require('gulp-concat'),
+        autoprefixer    = require('gulp-autoprefixer'),
+        rename          = require('gulp-rename'),
+        sourcemaps      = require('gulp-sourcemaps');
+
+const   browserSync     = require('browser-sync');
+
+gulp.task('build-sass', () => {
+    return gulp.src(['app/scss/build.sass'])
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browserSync', function(){
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+    })
+});
+
+gulp.task('go', ['browserSync', 'build-sass'], function(){
+    gulp.watch('app/scss/*',['build-sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/*.js', browserSync.reload);
+});
